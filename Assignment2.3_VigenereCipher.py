0cb4103568f43b5930f886c1b493c9d35849c4bc
Iris Membrere <irismembrere@gmail.com>
2023-04-19 10:58:00 +0800

    Comparing of the length of the 'message' and the 'key'

--- Assignment2.3_VigenereCipher.py
+++ Assignment2.3_VigenereCipher.py
@@ -7,3 +7,11 @@
 # Define function to generate keyword
 def Keyword(message, key):
     key = list(key)
+# If the length of the message == key, return key
+    if len(message) == len(key):
+        return key
+# If not, append the key
+    else:
+        for i in range (len(message) - len(key)):
+            key.append(key[i % len(key)])
+        return ("".join(key))
