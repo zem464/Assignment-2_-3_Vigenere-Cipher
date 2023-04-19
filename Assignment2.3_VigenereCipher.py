898fe51dfb3c1f14997b15769702eccab26a49d6
Iris Membrere <irismembrere@gmail.com>
2023-04-19 10:56:21 +0800

    Defining the function to generate keyword

---
+++ Assignment2.3_VigenereCipher.py
@@ -0,0 +1,9 @@
+# Zemerelin Iris M. Membrere
+# BSCPE 1-4
+# Assignment 2
+
+# Problem 3 - The Vigenere Cipher
+
+# Define function to generate keyword
+def Keyword(message, key):
+    key = list(key)
